c880ff3d05113c0c0dab5b336e0ab09246909db3
Sofia Deaza <sofiadeaza05@gmail.com>
2025-05-15 11:36:01 -0500

    scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
+    host: 'localhost',
+    port: 8080,
+    hot: false,
+  },
+};
